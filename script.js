3e086f09971ab44beaf5b86d3389e22d83fbde5f
codin-on <163760023+codin-on@users.noreply.github.com>
2024-03-18 00:46:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can add code to handle form submission, like sending data to a server
+    alert('Form submitted!');
+  });
+  
